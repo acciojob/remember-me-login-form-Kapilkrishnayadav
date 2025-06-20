5e20eb921286c4c4c059d3f9c2054450cb28aa40
Kapil Krishna Yadav <93436537+Kapilkrishnayadav@users.noreply.github.com>
2025-06-20 18:46:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 //your JS code here. If required.
+let submit=document.getElementById("submit");
+let username=document.getElementById("username");
+let password=document.getElementById("password");
+let checkbox=document.getElementById("checkbox");
+let existing=document.getElementById("existing");
+if(localStorage.getItem("username"))
+{
+    existing.style.display="inline-block";
+    console.log("hello");
+}
+else
+{
+    console.log("bye");
+    existing.style.display="none";
+}
+existing.addEventListener("click",()=>{
+    alert(`Logged in as ${localStorage.getItem("username")}`)
+})
+
+submit.addEventListener( "click",(event)=>{
+	event.preventDefault();
+    let name=username.value.trim();
+	
+	if(name=="" || password.value=="")
+	{
+		alert("Username or password field is empty");
+		 return
+	}
+	if(checkbox.checked)
+	{
+		localStorage.setItem("username",`${name}`);
+		localStorage.setItem("password",`${password.value}`);
+        existing.style.display="inline-block"
+        alert("credentials are stored in localStorage");
+		
+	}
+	else
+	{
+		localStorage.clear();
+        existing.style.display="none"
+        alert("Credentials are not stored");
+	}
+    
+	alert(`Logged in as ${name}`);
+    username.value="";
+    password.value="";
+})
